7d3b92e3d853c4962212ab6f6cf0e850c54746cc
William Elliott <wcelliott1991@gmail.com>
2017-09-19 14:35:58 -0400

    renders image to phone

--- App.js
+++ App.js
@@ -1,12 +1,21 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { 
+  StyleSheet, 
+  Text, 
+  View,
+  Image,
+} from 'react-native';
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
+        <Image 
+          source={{uri: 'https://www.noupe.com/wp-content/uploads/trans/wp-content/uploads/2010/02/waves_3.jpg'}}
+          style={{ width: 275, height: 175, margin: 20}}
+        />
         <Text>Hi Max!!!</Text>
-        <Text>Changes you make will automatically reload.</Text>
+        <Text>This is my favorite demo.</Text>
         <Text>Shake your phone to open the developer menu.</Text>
       </View>
     );

6bdb5fa18b80f86b1ed908f8375c4861453f0614
William Elliott <wcelliott1991@gmail.com>
2017-09-19 15:10:27 -0400

    successfully implements textinput to mobile!

--- App.js
+++ App.js
@@ -1,12 +1,17 @@
 import React from 'react';
-import { 
+import {
   StyleSheet, 
-  Text, 
+  Text,
+  TextInput,
   View,
   Image,
 } from 'react-native';
 
 export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state={text: 'How cool is this?'};
+  }
   render() {
     return (
       <View style={styles.container}>
@@ -17,6 +22,11 @@ export default class App extends React.Component {
         <Text>Hi Max!!!</Text>
         <Text>This is my favorite demo.</Text>
         <Text>Shake your phone to open the developer menu.</Text>
+        <TextInput
+          style={{height: 40, width: 200, borderColor: 'gray', borderWidth: 1, margin: 20}}
+          onChangeText={(text) => this.setState({text})}
+          value={this.state.text}
+        />
       </View>
     );
   }
@@ -30,3 +40,6 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
+
+

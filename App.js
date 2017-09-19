66b3cd6ae7a4df44d88251991f555476ddf32171
William Elliott <wcelliott1991@gmail.com>
2017-09-19 16:49:52 -0400

    inital WebView link

--- App.js
+++ App.js
@@ -5,26 +5,37 @@ import {
   TextInput,
   ScrollView,
   Image,
+  View,
+  WebView,
 } from 'react-native';
 
 export default class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state={text: 'How cool is this?'};
+    this.state={
+      text: 'How cool is this?',
+    };
   }
   render() {
     return (
       <ScrollView contentContainerStyle={styles.contentContainer} keyboardDismissMode='interactive'>
-        <Image 
-          source={{uri: 'http://bestanimations.com/Nature/Water/wave-ocean-animated-gif-6.gif'}}
-          style={{ width: 275, height: 175, margin: 20}}
+        <Text style={styles.header}>
+          William Elliott
+        </Text>
+        <WebView
+          source={{uri: 'http://williamclarkelliott.com'}}
+          style={{ width: 275, height: 100, marginTop: 20, flex: 1}}
         />
+        {/* <Image 
+          source={{uri: 'http://bestanimations.com/Nature/Water/wave-ocean-animated-gif-6.gif'}}
+          style={{ height: 150, width: 275}}
+        /> */}
         <Text>Hi Max!!!</Text>
         <Text>This is my favorite demo.</Text>
         <Text>Shake your phone to open the developer menu.</Text>
         <Text>{this.state.text}</Text>
         <TextInput
-          style={{height: 40, width: 200, borderColor: 'gray', borderWidth: 1, margin: 20}}
+          style={{height: 40, width: 200, borderColor: 'gray', borderWidth: 1, margin: 20, backgroundColor: 'white'}}
           onChangeText={(text) => this.setState({text})}
           value={this.state.text}
         />
@@ -36,10 +47,15 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   contentContainer: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#56baf8',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  header: {
+    marginTop: 35,
+    fontSize: 20,
+    fontWeight: 'bold',
+  }
 });
 
 
